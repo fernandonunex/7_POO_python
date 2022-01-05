6f19cd857c4dbddb3b0ab65ba5d8d6cc255fc8ae
fernandonunex <fer.bsof@outlook.com>
2022-01-05 13:29:27 -0600

    Video 3 of 9. YouTube Corey Schafer

--- classes_and_instances.py
+++ classes_and_instances.py
@@ -16,6 +16,21 @@ class Employee:
 
     def apply_raise(self):
         self.pay = self.raise_amount * self.pay
+    
+    @classmethod
+    def set_raise_amount(cls, amount):
+        cls.raise_amount = amount
+
+    @classmethod
+    def from_string(cls, emp_str):
+        first, last, pay = emp_str.split('-')
+        return cls(first, last, pay)
+
+    @staticmethod
+    def is_workday(day):
+        if day.weekday() == 5 or day.weekday() == 6:
+            return False
+        return True
 
 #---------------------Instances-----------------------
 emp_1 = Employee('Fernando', 'Nunez', 70000)
@@ -41,10 +56,27 @@ emp_2 = Employee('Eric', 'Ries', 1000000)
 # print(emp_1.num_of_emps)
 # print(emp_2.num_of_emps)
 
-print(emp_1.pay)
-print(emp_2.pay)
-emp_1.apply_raise()
-print(emp_1.pay)
-print(emp_2.pay)
 
+# print(emp_1.pay)
+# print(emp_2.pay)
+# emp_1.apply_raise()
+# print(emp_1.pay)
+# print(emp_2.pay)
 
+#----------Classmethods and staticmethods------------
+# Employee.set_raise_amount(1.08)
+
+# print(Employee.raise_amount)
+# print(emp_1.raise_amount)
+# print(emp_2.raise_amount)
+
+#Creating an alternative to constructor of a class
+# new_emp_3 = Employee.from_string('Juan-Munson-80000')
+
+# print(new_emp_3.email)
+# print(new_emp_3.pay)
+
+import datetime
+my_date = datetime.date(2022, 1, 10)
+
+print(Employee.is_workday(my_date))
